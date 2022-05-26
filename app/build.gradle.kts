36fed0868140c73696f6161a9d455fc945233f4d
Oleg Okhotnikov <olegnet@gmail.com>
2022-05-26 12:19:11 +0300

    Added Firebase Analytics to deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 27
         targetSdk = 32
 
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -139,6 +139,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:$navigationVersion")
 
     implementation(platform("com.google.firebase:firebase-bom:30.0.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
     implementation("com.google.firebase:firebase-perf-ktx")
 
