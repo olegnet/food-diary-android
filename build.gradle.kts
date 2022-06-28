49d94e864085c88f9145b099b491729c5e0e40db
Oleg Okhotnikov <olegnet@gmail.com>
2022-06-28 10:25:33 +0300

    Plugins updated to 7.4.0-alpha06, gradle to 7.5-rc-1, google services to 4.3.12, crashlytics to 2.9.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,14 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.3.10")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.0")
+        classpath("com.google.gms:google-services:4.3.12")
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.1")
         classpath("com.google.firebase:perf-plugin:1.4.1")
     }
 }
 
 plugins {
-    id("com.android.application") version "7.4.0-alpha03" apply false
-    id("com.android.library") version "7.4.0-alpha03" apply false
+    id("com.android.application") version "7.4.0-alpha06" apply false
+    id("com.android.library") version "7.4.0-alpha06" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
 }
