ef5f1c132fa9003b61739da749a6d649d8a1fc64
Oleg Okhotnikov <olegnet@gmail.com>
2023-01-09 10:38:52 +0300

    plugins updated to 8.0.0-alpha11

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.0.0-alpha06" apply false
-    id("com.android.library") version "8.0.0-alpha06" apply false
+    id("com.android.application") version "8.0.0-alpha11" apply false
+    id("com.android.library") version "8.0.0-alpha11" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
     kotlin("plugin.serialization") version "1.7.20" apply false
 }
