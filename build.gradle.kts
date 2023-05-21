d81bab126c7e35e9ad16010e9fba65382e91d175
Oleg Okhotnikov <olegnet@gmail.com>
2023-05-21 14:59:20 +0300

    Added comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ buildscript {
 plugins {
     id("com.android.application") version "8.2.0-alpha04" apply false
     id("com.android.library") version "8.2.0-alpha04" apply false
+
+    // When changing Kotlin version, also check 'kotlinCompilerExtensionVersion' in app/build.gradle.kts
+    // https://developer.android.com/jetpack/androidx/releases/compose-kotlin
     id("org.jetbrains.kotlin.android") version "1.8.21" apply false
     kotlin("plugin.serialization") version "1.8.21" apply false
     id("com.google.devtools.ksp") version "1.8.21-1.0.11" apply false
